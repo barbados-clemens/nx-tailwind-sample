ea4765d8d47eec725c5a2db5409504ffcc8e9100
Caleb Ukle <caleb.ukle+github@pm.me>
2023-03-07 13:05:17 -0600

    add react-vite with Tailwind and CT

---
+++ apps/react-vite/src/app/app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return <div className="bg-indigo-500 p-2 font-mono">Hello!</div>;
+}
+
+export default App;
